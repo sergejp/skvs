d2fe0a8f08969e68528b0b21feb8736c8800f84d
Sergej Pershaj <sergej.pershaj@pm.me>
2022-10-13 10:15:59 +0100

    Add default impl for KvStore

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,25 @@
-pub struct KvStore {
+pub struct KvStore {}
+
+impl Default for KvStore {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 impl KvStore {
-  pub fn new() -> Self {
-    KvStore {}
-  }
+    pub fn new() -> Self {
+        KvStore {}
+    }
 
-  pub fn set(&mut self, key: String, val: String) {
-    
-  }
+    pub fn set(&mut self, key: String, val: String) {
+        unimplemented!();
+    }
 
-  pub fn get(&self, key: String) -> Option<String> {
-    None
-  }
+    pub fn get(&self, key: String) -> Option<String> {
+        None
+    }
 
-  pub fn remove(&mut self, key: String) {
-  }
+    pub fn remove(&mut self, key: String) {
+        unimplemented!();
+    }
 }
