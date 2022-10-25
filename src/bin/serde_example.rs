38873caa19ace08d16db8ac9b1eec753223bdc9f
Sergej Pershaj <sergej.pershaj@pm.me>
2022-10-25 14:03:28 +0100

    Add example of BSON to bytes vec and back

--- src/bin/serde_example.rs
+++ src/bin/serde_example.rs
@@ -21,7 +21,8 @@ enum Direction {
 fn main() -> Result<(), Box<dyn Error>> {
     json_example()?;
     ron_example()?;
-    bson_example()?;
+    bson_file_example()?;
+    bson_vec_example()?;
     Ok(())
 }
 
@@ -68,7 +69,7 @@ fn ron_example() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn bson_example() -> Result<(), Box<dyn Error>> {
+fn bson_file_example() -> Result<(), Box<dyn Error>> {
     let mut f = File::create("db.bson")?;
 
     for i in 1..=1000 {
@@ -115,6 +116,46 @@ fn bson_example() -> Result<(), Box<dyn Error>> {
 
     println!("moves.len() = {}", &moves.len());
     println!("moves[..10] = {:?}", &moves[..10]);
+
+    Ok(())
+}
+
+fn bson_vec_example() -> Result<(), Box<dyn Error>> {
+    let mut moves_buf = Vec::<u8>::new();
+    for i in 1..=1000 {
+        let a = Move {
+            steps: i,
+            dir: Direction::Left,
+        };
+        let mut a_bson = bson::to_vec(&a)?;
+        let data_len = a_bson.len() as u32;
+        let mut data_frame =
+            Vec::<u8>::with_capacity(mem::size_of_val(&data_len) + data_len as usize);
+        data_frame = data_len.to_be_bytes().to_vec();
+        data_frame.append(&mut a_bson);
+        moves_buf.append(&mut data_frame);
+    }
+
+    println!(
+        "BSON VEC SERIALIZATION Moves[..10] = {:?}",
+        &moves_buf[..10]
+    );
+
+    let mut cursor = 0;
+    let mut moves = Vec::<Move>::with_capacity(1000);
+    while cursor < moves_buf.len() {
+        // read the data length from first 4 bytes
+        let data_len_buf = &moves_buf[cursor..(cursor + mem::size_of::<u32>())];
+        let data_len = u32::from_be_bytes(data_len_buf.try_into()?);
+        cursor += mem::size_of::<u32>();
+        let data = &moves_buf[cursor..(cursor + data_len as usize)];
+        cursor += data_len as usize;
+        let a: Move = bson::from_slice(&data.to_vec())?;
+        moves.push(a);
+    }
+
+    println!("BSON VEC DESERIALIZATION moves.len() = {}", &moves.len());
+    println!("BSON VEC DESERIALIZATION moves[..10] = {:?}", &moves[..10]);
 
     Ok(())
 }
